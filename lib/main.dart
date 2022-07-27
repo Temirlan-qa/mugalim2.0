17fe3e2779124a989c92bb2539081d323f1fb0bf
Temirlan-qa <temirlanaazbek@gmail.com>
2022-07-27 22:00:38 +0600

    tima

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mugalim/presentation/main/done.dart';
 import 'package:mugalim/presentation/main/select_jenre.dart';
 
 import 'core/routes/routes.dart';
@@ -9,7 +8,6 @@ void main() {
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
