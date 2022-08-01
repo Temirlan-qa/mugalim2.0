1f8466665bcb87eed61e9a856b66176479be49f8
Temirlan-qa <temirlanaazbek@gmail.com>
2022-08-01 17:36:35 +0600

    tima

--- lib/presentation/main/welcome_screen.dart
+++ lib/presentation/main/welcome_screen.dart
@@ -14,64 +14,66 @@ class WelcomeScreen extends StatelessWidget {
         ),
         color: Colors.white,
         child: SafeArea(
-          child: Column(
-            children: [
+          child: Center(
+            child: Column(
+              children: [
 
-              Image.asset(
-                  'assets/images/MugalimLogo.png',
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 16,
-                  left: 16,
-                  right: 16,
+                Image.asset(
+                    'assets/images/MugalimLogo.png',
+                  fit: BoxFit.cover,
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                        'Mugalim мобильді',
-                      style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.w700,
-                        height: 1.3,
-                        fontFamily: 'CeraPro',
-                      ),
-                    ),
-                    Text(
-                      'қосымшасына қош келдіңіз!',
-                      style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.w700,
-                        height: 1.3,
-                        fontFamily: 'CeraPro',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              Spacer(),
-              ElevatedButton(
-                onPressed: (){},
-                child: Text(
-                  'Әрі қарай >',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'CeraPro',
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 16,
+                    left: 16,
+                    right: 16,
                   ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(343, 48),
-                  primary: Color(0xFF3D3DD8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12), // <-- Radius
+                  child: Column(
+                    children: [
+                      Text(
+                          'Mugalim мобильді',
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w700,
+                          height: 1.3,
+                          fontFamily: 'CeraPro',
+                        ),
+                      ),
+                      Text(
+                        'қосымшасына қош келдіңіз!',
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w700,
+                          height: 1.3,
+                          fontFamily: 'CeraPro',
+                        ),
+                      ),
+                    ],
                   ),
                 ),
 
-              ),
-            ],
+                Spacer(),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text(
+                    'Әрі қарай >',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'CeraPro',
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(343, 48),
+                    primary: Color(0xFF3D3DD8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12), // <-- Radius
+                    ),
+                  ),
+
+                ),
+              ],
+            ),
           ),
         ),
       ),
