d764431c106fe5d313dc2ea1146a3c6025652d24
Temirlan-qa <temirlanaazbek@gmail.com>
2022-08-23 15:28:24 +0600

    tima fix

--- lib/presentation/books/screens/select_jenre.dart
+++ lib/presentation/books/screens/select_jenre.dart
@@ -587,6 +587,7 @@ class _JenreScreenState extends State<JenreScreen> {
                           color: Colors.white,
                         ),
                       )
+
                     : Container(
                         height: 24,
                         width: 24,
