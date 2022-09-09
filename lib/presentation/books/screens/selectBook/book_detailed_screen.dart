f7f54c3c90124d12828b427541c254f630d4f504
komik000 <200103436@stu.sdu.edu.kz>
2022-09-09 11:09:03 +0600

    BackEnd

--- lib/presentation/books/screens/selectBook/book_detailed_screen.dart
+++ lib/presentation/books/screens/selectBook/book_detailed_screen.dart
@@ -2,21 +2,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mugalim/core/const/const_color.dart';
-import 'package:mugalim/core/routes/routes_const.dart';
-import 'package:mugalim/presentation/books/screens/done.dart';
-import 'package:mugalim/presentation/books/screens/select_jenre.dart';
+import 'package:mugalim/logic/book/data/models/book_list_model.dart';
+import 'package:mugalim/presentation/books/screens/selectBook/done.dart';
+import 'package:mugalim/presentation/books/screens/selectBook/select_jenre.dart';
+import 'package:dio/dio.dart';
+import '../../../../core/injection_container.dart';
+import '../../../../logic/book/data/datasources/book_datasources.dart';
 
+// ignore: must_be_immutable
 class BookDescriptionScreen extends StatefulWidget {
   BookDescriptionScreen(
       {Key? key,
-      required this.index_month,
+      required this.indexMonth,
       required this.list,
-      required this.select_index})
-      : super(key: key);
-  int index_month;
+      required this.selectIndex,
+        required this.selectId,
+        required this.model,
+      }) : super(key: key);
+  int indexMonth;
   List list;
-  String select_index;
-
+  final BookListModel model;
+  String selectIndex;
+  String selectId;
   @override
   State<BookDescriptionScreen> createState() => _BookDescriptionScreenState();
 }
@@ -69,7 +76,7 @@ class _BookDescriptionScreenState extends State<BookDescriptionScreen> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Center(
                           child: ClipRRect(
                               borderRadius: BorderRadius.circular(16),
@@ -77,81 +84,64 @@ class _BookDescriptionScreenState extends State<BookDescriptionScreen> {
                                   width: 140, height: 220, fit: BoxFit.cover)
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Text(
-                          'Создавая инновации. Креативное мышление',
-                          style: TextStyle(
+                          widget.model.name!,
+                          style: const TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
-                        SizedBox(height: 4),
+                        const SizedBox(height: 4),
                         Text(
-                          'Даер Джефф, Клейтон М. Кристенсен',
+                          widget.model.authors[0]!.fio!,
                           style: TextStyle(
                             fontSize: 14,
                             fontWeight: FontWeight.w700,
                             color: ColorStyles.neutralsTextTertiaryColor,
                           ),
                         ),
-                        SizedBox(height: 8),
+                        const SizedBox(height: 8),
                         Row(
                           children: [
-                            Text(
+                            const Text(
                               '4.0',
                               style: TextStyle(
                                 fontSize: 18,
                                 fontWeight: FontWeight.w500,
                               ),
                             ),
-                            SizedBox(
+                            const SizedBox(
                               width: 7,
                             ),
                             SvgPicture.asset('assets/icons/star_full.svg'),
-                            SizedBox(
+                            const SizedBox(
                               width: 8,
                             ),
                             SvgPicture.asset('assets/icons/star_full.svg'),
-                            SizedBox(
+                            const SizedBox(
                               width: 8,
                             ),
                             SvgPicture.asset('assets/icons/star_full.svg'),
-                            SizedBox(
+                            const SizedBox(
                               width: 8,
                             ),
                             SvgPicture.asset('assets/icons/star_full.svg'),
-                            SizedBox(
+                            const SizedBox(
                               width: 8,
                             ),
                             SvgPicture.asset('assets/icons/star_empty.svg'),
-                            // RatingBar(
-                            //   initialRating: state.book.rating!,
-                            //   direction: Axis.horizontal,
-                            //   allowHalfRating: true,
-                            //   itemCount: 5,
-                            //   itemSize: 18,
-                            //   ratingWidget: RatingWidget(
-                            //     full: SvgPicture.asset('assets/icons/star_full.svg'),
-                            //     half: SvgPicture.asset('assets/icons/star_half.svg'),
-                            //     empty: SvgPicture.asset('assets/icons/star_empty.svg'),
-                            //   ),
-                            //   itemPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                            //   ignoreGestures: true,
-                            //   onRatingUpdate: (rating) {
-                            //     print(rating);
-                            //   },
-                            // )
                           ],
                         ),
-                        SizedBox(height: 16),
-                        Text(
+                        const SizedBox(height: 16),
+                        const Text(
                           "Описание",
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
-                        SizedBox(height: 6),
+                        const SizedBox(height: 6),
                         Text(
                           'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended for adu lts but is often read by adolescents for its theme of angst, alienation and as a critique......',
                           style: TextStyle(
@@ -163,7 +153,7 @@ class _BookDescriptionScreenState extends State<BookDescriptionScreen> {
                             // color: ColorStyles.neutralsTextTertiaryColor
                           ),
                         ),
-                        Spacer(),
+                        const Spacer(),
                       ],
                     ),
                   ),
@@ -171,56 +161,54 @@ class _BookDescriptionScreenState extends State<BookDescriptionScreen> {
               ],
             ),
             Positioned(
-              bottom: 16,
-              child: Container(
+              bottom: 50,
+              child: SizedBox(
                 width: MediaQuery.of(context).size.width,
                 child: Center(
                   child: TextButton(
-                      child: Text(
+                      style: TextButton.styleFrom(
+                        primary: const Color(0xFFE0E0E0),
+                        backgroundColor: const Color(0xff3D3DD8),
+                        elevation: 3,
+                        minimumSize: const Size(343, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (widget.indexMonth.toInt() >= 3) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ChoosenPage()
+                            ),
+                          );
+                        }
+                        if (widget.list.isNotEmpty &&
+                            widget.indexMonth.toInt() < 3) {
+                          widget.list.remove(widget.selectIndex);
+                          final BookDatasource bookDatasource = sl();
+                          Response response = await bookDatasource.postVote(widget.selectId,widget.model.id!);
+                          // ignore: use_build_context_synchronously
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => JenreScreen(
+                                indexMonth: widget.indexMonth.toInt() + 1,
+                                list: widget.list,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text(
                         "Выбрать книгу",
                         style: TextStyle(
                           fontSize: 16,
                           fontFamily: 'CeraPro',
                           fontWeight: FontWeight.w500,
                         ),
-                      ),
-                      style: TextButton.styleFrom(
-                        primary: Color(0xFFE0E0E0),
-                        backgroundColor: Color(0xff3D3DD8),
-                        elevation: 3,
-                        minimumSize: Size(343, 48),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                      ),
-                      onPressed: () {
-                        if (widget.index_month.toInt() >= 3) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ChoosenPage()
-                            ),
-                          );
-                        }
-                        if (!widget.list.isEmpty &&
-                            widget.index_month.toInt() < 3) {
-                          widget.list.remove(widget.select_index);
-                          // Navigator.pushReplacementNamed(context, JenreRoute,
-                          //     arguments: {
-                          //       index_month: widget.index_month.toInt() + 1,
-                          //       list: widget.list,
-                          //     });
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => JenreScreen(
-                                index_month: widget.index_month.toInt() + 1,
-                                list: widget.list,
-                              ),
-                            ),
-                          );
-                        }
-                      }),
+                      )),
                 ),
               ),
             ),
